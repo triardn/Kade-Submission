6f30963a8cb37286f4901bf466a866b3c77a9c17
Tri Ardini <tr.ardn@gmail.com>
2020-01-21 23:27:55 +0700

    instrumented test search match:

--- app/src/androidTest/java/com/triardn/kadesubmission/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/triardn/kadesubmission/ExampleInstrumentedTest.kt
@@ -1,7 +1,10 @@
 package com.triardn.kadesubmission
 
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
+//import androidx.test.platform.app.InstrumentationRegistry
+//import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
--- app/src/androidTest/java/com/triardn/kadesubmission/SearchMatchTest.kt
+++ app/src/androidTest/java/com/triardn/kadesubmission/SearchMatchTest.kt
@@ -1,15 +1,14 @@
 package com.triardn.kadesubmission
 
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.clearText
-import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.runner.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.util.regex.Pattern.matches
 
 @RunWith(AndroidJUnit4::class)
 class SearchMatchTest {
@@ -19,14 +18,13 @@ class SearchMatchTest {
     @Test
     fun testSearchMatch() {
         // search
-        onView(withId(R.id.action_search_menu)).check(matches(isDisplayed()))
-        onView(withId(R.id.action_search_menu)).check(matches(isDisplayed()))
-        onView(withId(R.id.action_search_menu)).perform(click())
-        onView(withText("Barcelona")).perform(click())
-        clearText()
-        onView(withText("Chelsea")).perform(click())
-        clearText()
-        onView(withText("Arsenal")).perform(click())
-        clearText()
+        onView(withId(R.id.search_query)).check(matches(isDisplayed()))
+        onView(withId(R.id.search_query)).perform(click())
+        onView(withId(R.id.search_query)).perform(typeText("Barcelona"))
+        onView(withId(R.id.search_query)).perform(clearText())
+        onView(withId(R.id.search_query)).perform(typeText("Arsenal"))
+        onView(withId(R.id.search_query)).perform(clearText())
+        onView(withId(R.id.search_query)).perform(typeText("Chelsea"))
+        onView(withId(R.id.search_query)).perform(clearText())
     }
 }
